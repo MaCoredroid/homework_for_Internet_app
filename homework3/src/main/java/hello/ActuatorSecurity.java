e2642168e567a04c8f4930de1f3e7cdc5a77ed58
unknown <coredroid0401@gmail.com>
2019-05-01 20:04:36 +0800

    emmm

--- homework3/src/main/java/hello/ActuatorSecurity.java
+++ homework3/src/main/java/hello/ActuatorSecurity.java
@@ -3,7 +3,6 @@ package hello;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -12,7 +11,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@Order(101)
 public class ActuatorSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
