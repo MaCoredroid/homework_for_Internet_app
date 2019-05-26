0aee103ebc4947bef5dc6cefab2297ff22e7d7a3
macoredroid <coredroid0401@gmail.com>
2019-05-26 19:03:57 +0800

    edit producer

--- task4/product-consumer/src/producer.java
+++ task4/product-consumer/src/producer.java
@@ -7,13 +7,14 @@ public class producer extends TimerTask {
     private int threshold;
     private int timeout;
     private Timer alarm;
+    private boolean overload=false;
     private List<consumer> container;
     public producer (int threshold,int timeout)
     {
         this.threshold=threshold;
         this.timeout=timeout;
     }
-    public int addConsumer(int number)
+    public int addConsumer(int number)//add task
     {
         for(int i=0;i<number;i++)
         {
@@ -26,10 +27,25 @@ public class producer extends TimerTask {
     @Override
     public void run()
     {
-        
-        if(container.size()>threshold)
-        {
 
+        if(container.size()>=threshold)
+        {
+             //deal with the newer tasks first
+            this.overload=true;
         }
+        else
+        {
+            //deal with the older task first
+            this.overload=false;
+        }
+        for(consumer temp:container)
+        {
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            if (timestamp.getTime()-temp.getTimestamp().getTime()>=timeout)
+            {
+                container.remove(temp);
+            }
+        }
+        System.out.println("There remains "+container.size()+"consumers");
     }
 }
