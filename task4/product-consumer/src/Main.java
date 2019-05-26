6d8d3b1fd248e3f01ea65da700cfa70d4f1fb585
macoredroid <coredroid0401@gmail.com>
2019-05-26 19:10:35 +0800

    fix

--- task4/product-consumer/src/Main.java
+++ task4/product-consumer/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         Timer time = new Timer();
         producer p = new producer(50, 1000);
         time.schedule(p, 0, 10);
+        p.addConsumer(50);
     }
 }
--- task4/product-consumer/src/producer.java
+++ task4/product-consumer/src/producer.java
@@ -16,36 +16,35 @@ public class producer extends TimerTask {
     }
     public int addConsumer(int number)//add task
     {
-        for(int i=0;i<number;i++)
-        {
-            consumer temp = new consumer(new Timestamp(System.currentTimeMillis()));
-            container.add(temp);
+        if(container!=null) {
+            for (int i = 0; i < number; i++) {
+                consumer temp = new consumer(new Timestamp(System.currentTimeMillis()));
+               container.add(temp);
+
+            }
+            return container.size();
         }
-        return container.size();
+        return 0;
     }
 
     @Override
     public void run()
     {
-
-        if(container.size()>=threshold)
-        {
-             //deal with the newer tasks first
-            this.overload=true;
-        }
-        else
-        {
-            //deal with the older task first
-            this.overload=false;
-        }
-        for(consumer temp:container)
-        {
-            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-            if (timestamp.getTime()-temp.getTimestamp().getTime()>=timeout)
-            {
-                container.remove(temp);
+        if (container != null) {
+            if (container.size() >= threshold) {
+                //deal with the newer tasks first
+                this.overload = true;
+            } else {
+                //deal with the older task first
+                this.overload = false;
             }
+            for (consumer temp : container) {
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                if (timestamp.getTime() - temp.getTimestamp().getTime() >= timeout) {
+                    container.remove(temp);
+                }
+            }
+            System.out.println("There remains " + container.size() + "consumers");
         }
-        System.out.println("There remains "+container.size()+"consumers");
     }
 }
